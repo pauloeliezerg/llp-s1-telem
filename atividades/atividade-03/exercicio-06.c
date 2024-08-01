07688577a368e72bf5c487d68feae192805662a1
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-01 14:17:25 -0300

    adicionado o exericio-06 da atividade-03

---
+++ atividades/atividade-03/exercicio-06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// 1. Atividade com Array Unidimensional:
+// - Descrição: Escreva um programa que leia 5 números inteiros do usuário e armazene-os em um array. Em seguida, exiba a soma desses números.
+// - Dica: Use um loop `for` para ler os números e outro loop para calcular a soma.
+
+// 2. Atividade com Array Multidimensional (Matriz):
+// - Descrição: Crie um programa que leia uma matriz 3x3 (ou seja, uma matriz com 3 linhas e 3 colunas) e exiba a soma dos elementos da diagonal principal.
+// - Dica: Use um loop `for` aninhado para ler os elementos da matriz e calcule a soma dos elementos da diagonal principal.
+
+int main() {
+  return 0;
+}

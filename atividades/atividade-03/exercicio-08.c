66f6c021e9e6cab7b9dec7b0e9003bfc195542ac
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-01 14:57:28 -0300

    adicionado o exericio-08 da atividade-03

---
+++ atividades/atividade-03/exercicio-08.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// 1. Calculadora Simples
+// - Crie um programa que leia dois números inteiros do usuário e exiba a soma, subtração, multiplicação e divisão desses números.
+
+// 2. Conversão de Temperatura
+// - Escreva um programa que converta uma temperatura em graus Celsius para Fahrenheit. O usuário deve inserir a temperatura em Celsius.
+
+// 3. Exercício: Verificação de Número Par
+// - Peça ao usuário para digitar um número inteiro. Verifique se o número é par ou ímpar e exiba a mensagem correspondente.
+
+// 4. Cálculo de Média Ponderada
+// - Leia três notas (pesos 2, 3 e 5) e calcule a média ponderada. Exiba a média resultante.
+
+// 5. Troca de Valores
+// - Leia dois números inteiros e troque seus valores sem usar uma variável temporária.
+
+int main() {
+  return 0;
+}

dea7c0667ac4f35ec8f07f9626ada0881a177be8
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-01 13:51:53 -0300

    adicionado o exericio-05 da atividade-03

---
+++ atividades/atividade-03/exercicio-05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// 1. Atividade com `while`:
+// - Descrição: Escreva um programa que exiba os números pares de 1 a 10 usando um loop `while`.
+// - Dica: Use o operador de módulo (`%`) para verificar se um número é par.
+
+// 2. Atividade com `do while`:
+// - Descrição: Crie um programa que solicite ao usuário que adivinhe um número entre 1 e 100. O programa deve continuar pedindo até que o usuário acerte o número ou digite `-1` para sair.
+// - Dica: Use um loop `do while` para repetir a solicitação.
+
+// 3. Atividade com `for`:
+// - Descrição: Escreva um programa que calcule a soma dos primeiros 10 números inteiros positivos usando um loop `for`.
+// - Dica: Use uma variável acumuladora para calcular a soma.
+
+int main() {
+  return 0;
+}

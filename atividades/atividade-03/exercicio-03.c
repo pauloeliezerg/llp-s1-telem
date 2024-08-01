920eeeee2ec002762655a2f65f800d216c2196f1
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-01 04:51:09 -0300

    adicionado o exercicio-03 da atividade-03

---
+++ atividades/atividade-03/exercicio-03.c
@@ -0,0 +1,7 @@
+// 1. O que é Manipulação de Bits?
+
+// 2. Qual a Importância da Manipulação de Bits?
+
+// 3. Quais são as Operações Básicas de Manipulação de Bits?
+
+// 4. Exemplo em Linguagem C. Crie um programa  que exibe os bits de um número e realiza operações básicas.

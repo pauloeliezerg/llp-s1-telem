81c24b577d4b3b2633ecee648a90cc2e1a7bd34c
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-09-12 07:42:43 -0300

    adicionada atividade-04

---
+++ atividades/atvidade-04/atividade-04.c
@@ -0,0 +1,43 @@
+// Modificar o programa abaixo, de forma a implementar uma função verifica_aprovacao e uma função mostra_aprovacao.
+
+// A função verifica_aprovacao percorre o array de alunos declarados e definidos no código fazendo as seguintes verificações: 
+// 1)  Se o numero de faltas for maior ou igual a 20, considerar que o aluno será reprovado. Atualizar o campo Reprovado_SN com o valor 'S';
+// 2)  Calcular a Media fazendo o seguinte calculo (N1 + N2)/2. O resultado atualizará o campo Media.
+
+// A função mostra_aprovacao percorre o array de alunos já modificados e mostra os dados do aluno (matricula, nome, N1, N2, Faltas). Também apresentará as seguintes mensagens:
+// "REPROVADO POR FALTA" se o campo Reprovado_SN for igual a 'S';
+// "REPROVADO POR MEDIA" se o campo Media for menor que 6.0;
+// "APROVADO" se naõ atender as condições anteriores.
+
+#include <stdio.h>
+
+typedef struct {
+    int matricula;
+    char nome[20];
+    float N1;
+    float N2;
+    float Media;
+    int Faltas;
+    char Reprovado_SN;
+} Aluno;
+
+int main() {
+    Aluno alunos[5] = {
+        {1, "João", 8.5, 9.0, 0.0, 25,' '},
+        {2, "Maria", 9.0, 8.0, 0.0, 1,' '},
+        {3, "Pedro", 7.5, 8.5, 0.0, 3,' '},
+        {4, "Ana", 9.5, 9.5, 0.0, 20,' '},
+        {5, "Luiz", 8.0, 7.0, 0.0, 4,' '}
+    };
+
+    for (int i = 0; i < 5; i++) {
+        printf("Aluno %d:\n", i + 1);
+        printf("Matrícula: %d\n", alunos[i].matricula);
+        printf("Nome: %s\n", alunos[i].nome);
+        printf("N1: %.2f\n", alunos[i].N1);
+        printf("N2: %.2f\n", alunos[i].N2);
+        printf("Faltas: %d\n\n", alunos[i].Faltas);
+    }
+
+    return 0;
+}

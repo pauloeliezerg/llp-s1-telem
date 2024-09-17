196d9d12afd04e500ab45b7e5fb8885c187b8324
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-09-17 20:02:14 -0300

    adicionado o enunciado para a atividade-05

---
+++ atividades/atividade-05/atividade-05.c
@@ -0,0 +1,18 @@
+// Escrever um programa em C que faz as seguintes operações:
+// A execução e o código fonte do programa deverão ser evidenciados no GitHub e o link do GitHub gravado no
+// Google Classroom.
+
+// 1) Cria um arquivo que armazena linhas de textos;
+// 2) O Arquivo será alimentado por dados digitados no teclado;
+// 3) Mostrar o conteúdo do arquivo. (A partir do passo 6)
+
+// Observações:
+// 1) Usar a função fopen para abrir um arquivo que será criado;
+// 2) Usar a funções de entrada de dados pelo teclado;
+// 3) Gravar a string lida do teclado no arquivo; 
+// 4) Executar a instrução anterior até uma condição final. Para isso usar um laço while;
+// 5) Fechar o arquivo;
+
+// 6) Abrir o arquivo no modo de leitura;
+// 7) Ler cada linha do arquivo e mostra-la no vídeo;
+// 8) Fechar o arquivo.

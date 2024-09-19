5c7c679f1bfca1f6766927b99d9e8ae576bb6d50
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-09-18 21:14:56 -0300

    adicionado o enunciado da atividade-06

---
+++ atividades/atividade-06/atividade-06.c
@@ -0,0 +1,31 @@
+// Fazer um programa em C para executar as seguintes funcionalidades: Mostrar uma tela de Menu que recebe um valor de opcao. De acordo com o valor entrado (1,2,3,4) fazer as seguintes  
+
+// 1) Escrever na Tela um Menu no formato abaixo:
+// ----------------------------------------------------------------------
+// Sistema de Estoque
+// ----------------------------------------------------------------------
+// 1) Entrada de Produtos
+// 2) Listar os Produtos
+// 3) Valor Total do Estoque
+// 4)  Fim
+// Opção: __
+// ---------------------------------------------------------------------
+
+// Opção 1
+// 1) Entrar os seguintes dados em um formato de acordo com os dados abaixo: 
+// ----------------------------------------------------------------------
+// Entrada de Cadastro de Produtos
+// ----------------------------------------------------------------------
+// Código: 9999
+
+// Descrição: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Valor Unitário: 99.999,99
+
+// Qtd Estoque: 999
+
+// Valor Estoque: 999.999,99  (Campo calculado ValorUnitario x QtdEstoque)
+
+// Digite (1-Para Gravar, 2-Voltar a tela inicial) : __
+
+// Quando for digitado 1 gravar os dados em um array de struct de produtos.

e0a44e5bd5c89b20b129caabbb62e0a00fa8cfeb
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-09-21 20:43:54 -0300

    adicionado o enunciado da atividade-07

---
+++ atividades/atividade-07/atividade-07.c
@@ -0,0 +1,11 @@
+// Escreva um programa C compatível com o compilador C do ambiente Replit.com. O programa fará as operações a seguir:
+
+// O programa apresentará um menu na console de saída. O menu consistirá de uma tela com opções a serem executadas. A entrada do valor de uma das opções do menu chamará uma funcionalidade correspondente em outra tela.
+// As opções do menu de são:
+
+// Cadastrar Produto. Nessa opção serão informados os seguintes dados para entrada na tela: codigo, descrição, valor unitario, quantidade estoque;
+// Consultar Produto para Alteração. Todos os dados citados anteriormente serão apresentados para serem alterados;
+// Listar todos os produtos. Nessa opção para cada produto é calculado o valor do estoque multiplicando a quantidade em estoque vezes o preco unitario;
+// Exclusão do Produto. Nessa opção a tela apresentará um código que será buscado e apresentará um produto. Na tela terá uma entrada de dados respondendo sim ou Não para confirmar exclusão do produto.
+// Observação: Os produtos serão armazenados em um array de struct de produtos.
+
